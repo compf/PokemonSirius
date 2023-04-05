ebd33217b5badde9691ee651569c65ebab87c63f
=compf <=>
2023-04-05 11:04:00 +0200

    irst step for battle menu

---
+++ godot_version/battle.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#$CanvasLayer/PopupMenu.popup(Rect2i(50,50,100,100))
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+
+	pass
