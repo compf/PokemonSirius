1e8b9dbdf10d7955189c78cd071a798a9654f4db
=compf <=>
2023-04-19 15:21:18 +0200

    transferrred moves to json and removed files

--- core/src/main/java/compf/core/engine/SharedInformation.java
+++ core/src/main/java/compf/core/engine/SharedInformation.java
@@ -16,6 +16,8 @@ import java.util.HashMap;
 
 import compf.core.engine.pokemon.*;
 import compf.core.engine.pokemon.moves.*;
+import compf.core.engine.pokemon.moves.Move.MoveKind;
+import netscape.javascript.JSObject;
 /**
  * @author timoc
  * @date 15.06.18
@@ -24,12 +26,11 @@ public final class SharedInformation {
 	public static final int[] PokemonGenCount = { 151, 251, 386, 493, 650, 722, 808 };
 	public static final int TeamSize = 6;
 	public static final int MovesPerPokemonCount = 4;
-	private static final String WIN_PATH = "jdbc:sqlite:veekun-pokedex.sqlite";
-	private static String PATH = WIN_PATH;
+
 	public static final int PokemonCount = 1020;
-	public static final int MovesCount = PokemonCount - 100;
+	public static final int MovesCount = PokemonCount ;
 	private HashMap<Integer,PokedexEntry> pokemon = new HashMap<>(PokemonCount);
-	private Move[] moves = new Move[MovesCount];
+	private HashMap<Integer,Move> moves =  new HashMap<>(MovesCount);
 	public static final SharedInformation Instance = new SharedInformation();
 
 	/**
@@ -43,17 +44,15 @@ public final class SharedInformation {
 
 		try {
 
-			Class.forName("org.sqlite.JDBC");
-			final String sqlString = "jdbc:sqlite:lib/veekun-pokedex.sqlite";
-			Connection conn = DriverManager.getConnection(sqlString);
-			System.out.println(System.getProperty("user.dir"));
+			
 			try(var reader=new FileReader("lib/pokedex.json")){
 				JsonObject convertedObject = new Gson().fromJson(reader, JsonObject.class);
 				initPokemon(convertedObject);
 			}
-
-			initMoves(conn);
-			conn.close();
+			try(var reader=new FileReader("lib/moves.json")){
+				JsonObject convertedObject = new Gson().fromJson(reader, JsonObject.class);
+				initMoves(convertedObject);
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -73,8 +72,6 @@ public final class SharedInformation {
 
 			float height = object.get("heightm").getAsFloat();
 			float weight = object.get("weightkg").getAsFloat();;
-			// reader.beforeFirst();
-			boolean firstType = true;
 			Type type1 = Type.Empty;
 			Type type2 = Type.Empty;
 			JsonArray typeArray=object.get("types").getAsJsonArray();
@@ -82,7 +79,6 @@ public final class SharedInformation {
 			if(typeArray.size()>1){
 				type2=Enum.valueOf(Type.class,typeArray.get(1).getAsString());
 			}
-			// reader.beforeFirst();
 			int hp, att, def, satt, sdef, speed;
 			var baseStats=object.get("baseStats").getAsJsonObject();
 			hp = baseStats.get("hp").getAsInt();
@@ -97,42 +93,34 @@ public final class SharedInformation {
 
 	}
 
-	private void initMoves(Connection conn) throws SQLException {
+	private void initMoves(JsonObject fullData) throws SQLException {
 		final MoveFactory factory = new MoveFactory();
-		PreparedStatement statement = conn.prepareStatement("SELECT * FROM moves WHERE ?=id");
-		for (int i = 1; i < MovesCount; i++) {
-			statement.setInt(1, i);
-			var reader = statement.executeQuery();
-			reader.next();
-			String name = reader.getString(2);
-			name = Character.toUpperCase(name.charAt(0)) + name.substring(1);
-			Type type = Type.parse(reader.getInt(4));
+		for (String name:fullData.keySet()) {
+			var object=fullData.get(name).getAsJsonObject();
+			Type type = Enum.valueOf(Type.class, object.get("type").getAsString());
 			if (type == null)
 				type = Type.Normal;
-			byte power = reader.getByte(5);
-			byte pp = reader.getByte(6);
-			byte accuracy = reader.getByte(7);
-			if (accuracy == 0) {
-				accuracy = 127;
+			byte power = object.get("basePower").getAsByte();
+			byte pp = object.get("pp").getAsByte();
+			String accuracyStr = object.get("accuracy").getAsString();
+			byte accuracy;
+			if (accuracyStr.contentEquals("true")) {
+				accuracy = 100;
+			}else{
+				accuracy=Byte.valueOf(accuracyStr);
 			}
-			byte priority = reader.getByte(8);
-			byte target = reader.getByte(9);
+			byte priority = object.get("priority").getAsByte();
+			String target = object.get("target").getAsString();
 			if (priority >= 0)
 				priority++; // Prevent Zero in priority
 			else
 				priority--;
 
-			int damageClass = reader.getInt(10);
-			Move.MoveKind kind = null;
-			if (damageClass == 1)
-				kind = Move.MoveKind.Status;
-			else if (damageClass == 2)
-				kind = Move.MoveKind.Physical;
-			else
-				kind = Move.MoveKind.Special;
-			final Move mv = new Move(i, name, power, pp, accuracy, priority, type, kind, Move.TargetType.find(target));
-			moves[i - 1] = factory.create(mv);
-			reader.close();
+			String damageClass = object.get("category").getAsString();
+			Move.MoveKind kind = Enum.valueOf(MoveKind.class, damageClass);
+			int nr=object.get("num").getAsInt();
+			final Move mv = new Move(nr, name, power, pp, accuracy, priority, type, kind, null);
+			moves.put(nr,factory.create(mv));
 		}
 	}
 
@@ -141,9 +129,7 @@ public final class SharedInformation {
 	}
 
 	public Move getMove(int nr) {
-		if (nr == 0)
-			return null;
-		return moves[nr - 1];
+		return moves.get(nr);
 	}
 
 }
