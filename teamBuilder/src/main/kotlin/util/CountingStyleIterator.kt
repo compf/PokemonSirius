764d94fc0a60c783a9948070f4e1ebb54224e1c4
compf <=>
2023-05-30 13:22:38 +0200

    fixed missing file

---
+++ teamBuilder/src/main/kotlin/util/CountingStyleIterator.kt
@@ -0,0 +1,51 @@
+package util
+
+abstract class CountingStyleIterator<T> : Iterator<T> {
+    private var hasAnyNext = true;
+    private val numberIterators: Int;
+    protected val iterators: Array<Iterator<Any>?>
+
+     constructor(numberIterators: Int) {
+        this.numberIterators = numberIterators
+        this.iterators = arrayOfNulls<Iterator<Any>?>(numberIterators)
+
+    }
+     fun resetAll(){
+        for (i in 0 until numberIterators) {
+            reset(i)
+            if(iterators[i]!!.hasNext()){
+                assign(i,iterators[i]!!.next())
+            }
+
+        }
+    }
+
+    public abstract fun reset(index: Int);
+    public abstract  fun assign(index:Int,value:Any)
+    public abstract fun construct(): T;
+    override fun hasNext(): Boolean {
+        return hasAnyNext
+    }
+
+    override fun next(): T {
+        for (i in numberIterators-1 downTo 0) {
+            if (!iterators[i]!!.hasNext()) {
+                if(i==0){
+                    hasAnyNext=false
+                }
+                for(j in i until numberIterators){
+                    reset(j)
+                    if(iterators[j]!!.hasNext()){
+                        assign(j,iterators[j]!!.next())
+                    }
+
+                }
+            }
+            else{
+                assign(i,iterators[i]!!.next())
+                break
+            }
+        }
+        return construct()
+    }
+}
