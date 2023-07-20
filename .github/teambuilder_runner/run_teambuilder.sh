137c29ef00085cad55f955515848425df2d64603
compf <=>
2023-07-20 19:07:45 +0200

    moved from actions to workflow

--- .github/teambuilder_runner/run_teambuilder.sh
+++ .github/teambuilder_runner/run_teambuilder.sh
@@ -1,10 +1,4 @@
 #!/bin/sh -l
 
 gradle teamBuilder:run
-git config --global --add safe.directory /github/workspace
-git config --local user.email "actions@github.com"
-git config --local user.name "Github Actions"
-git checkout battlelog
-git add teamBuilder/*.txt
-git commit -m "added log"
-git push
+
