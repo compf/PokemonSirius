26c75a937497d2de224d4e60f9a45da4d76330e1
compf <=>
2023-07-20 18:58:04 +0200

    added exception for workspace

--- .github/teambuilder_runner/run_teambuilder.sh
+++ .github/teambuilder_runner/run_teambuilder.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 
 gradle teamBuilder:run
+git config --global --add safe.directory /github/workspace
 git config --local user.email "actions@github.com"
 git config --local user.name "Github Actions"
 git checkout battlelog
