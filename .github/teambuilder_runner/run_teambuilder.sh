96b8bf95cdb49de18ad58ab13654f3f6e348c83d
compf <=>
2023-07-20 18:55:01 +0200

    added git commands for pushing logs  to second branch

--- .github/teambuilder_runner/run_teambuilder.sh
+++ .github/teambuilder_runner/run_teambuilder.sh
@@ -1,3 +1,9 @@
 #!/bin/sh -l
 
 gradle teamBuilder:run
+git config --local user.email "actions@github.com"
+git config --local user.name "Github Actions"
+git checkout battlelog
+git add teamBuilder/*.txt
+git commit -m "added log"
+git push
